2148a5656c7b3c2d67d6d82315d2d830e4f14d49
zkdlu <zkdlu159951@gmail.com>
2020-10-27 17:41:17 +0900

    first commit

---
+++ src/test/java/com/zkdlu/redis/RedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkdlu.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
